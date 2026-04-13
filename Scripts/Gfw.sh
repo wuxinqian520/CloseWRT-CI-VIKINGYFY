ac94b0418f0f9acb849c61b0926e5b39469638a7
wuxinqian520 <52878878+wuxinqian520@users.noreply.github.com>
2026-04-13 12:18:41 +0800

    Update Gfw.sh

--- Scripts/Gfw.sh
+++ Scripts/Gfw.sh
@@ -40,9 +40,4 @@ UPDATE_PACKAGE() {
 	fi
 }
 
-UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
-UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
-UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
-UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
+UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main"
